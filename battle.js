ef9b87c06c1f7866e183490cfe259c8880368617
Route9Library <you@example.com>
2020-08-12 16:12:08 -0400

    done

--- battle.js
+++ battle.js
@@ -1,5 +1,5 @@
 
-var randomloc = Math.floor(Math.random() * 7)
+var randomloc = Math.floor(Math.random() * 9)
 var location1 = randomloc;
 var location2 = location1 + 1;
 var location3 = location2 + 1;
@@ -9,9 +9,9 @@ var guesses = 0;
 var isSunk = false;
 
 while (isSunk == false){
-    guess = prompt("Ready, aim, fire! (enter a number from 0-6):")
+    guess = prompt("Ready, aim, fire! (enter a number from 0-10):")
 
-    if (guess < 0 || guess > 6) {
+    if (guess < 0 || guess > 10) {
         alert("Please enter a valid cell number!");
     } else {
         guesses = guesses + 1; 
@@ -32,19 +32,6 @@ while (isSunk == false){
     var stats = "You took " + guesses + " guesses to sink the battleship, " +
     "which means your shooting accuracy was " + (3/guesses); 
     alert(stats);
-
-
-
-// function restart() {
-//     var txt;
-//     var person = prompt("Do You Want To Play again? Type Yes, if not Type No");
-//     if(person == Yes || person == "Refresh Page") {
-        
-//     } else{
-//         (person == "Mayebe Later");
-//     }
-//     document.getElementById("battle").battle.html = txt;
-// }
 
 function replay(){
     location.reload();
